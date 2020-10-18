30b1a1f45521e90d7203bd2d50b82774422af1aa
Getu Gebre <getu.gebre.gg@gmail.com>
2020-10-17 21:42:10 -0500

    done

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,4 +1,15 @@
 require 'nokogiri'
 require 'open-uri'
-
+require 'pry'
 html = open("https://flatironschool.com/")
+doc = Nokogiri::HTML(html)
+
+# binding.pry
+
+courses = doc.css(".title-oE5vT4")
+
+courses.select do |course|
+    course.text.strip.split(' ').size == 2
+end.each do |course|
+    puts course.text.strip
+end
